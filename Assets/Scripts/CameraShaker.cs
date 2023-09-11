fdde4fcceb5c6c5495c5cbf55dd9018ae4d44977
Kirdankorabel <74896893+Kirdankorabel@users.noreply.github.com>
2023-09-11 23:40:00 +0300

    camera shaker

---
+++ Assets/Scripts/CameraShaker.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using UnityEngine;
+
+public class CameraShaker : MonoBehaviour
+{
+    [SerializeField] private float _shakeDuration = 0.5f;
+    [SerializeField] private float _shakeMagnitude = 0.1f; 
+
+    private void Start()
+    {
+        Holder.OnHolderCollision += () => StartCoroutine(ShakeCorutine());
+    }
+
+    private IEnumerator ShakeCorutine()
+    {
+        var originalPosition = transform.localPosition;
+        var currentShakeDuration = _shakeDuration;
+        while (currentShakeDuration > 0)
+        {
+            Vector3 shakeOffset = Random.insideUnitSphere * _shakeMagnitude;
+            transform.localPosition = originalPosition + shakeOffset;
+
+            currentShakeDuration -= Time.deltaTime;
+            yield return null;
+        }
+        transform.localPosition = originalPosition;
+    }
+}
